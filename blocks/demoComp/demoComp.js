544f0e95992de9f141810294b1df2d295a59c5d8
indulekha99 <indulekhask9@gmail.com>
2025-02-19 21:52:26 +0530

    demo comp correction

--- blocks/demoComp/demoComp.js
+++ blocks/demoComp/demoComp.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
     console.log('block is',block);
-    const h1 = document.createElement('h1');
-    h1.textContent='hello';
-    block.innerHTML = '';
+    const h1 = document.createElement("h1");
+    h1.textContent="hello";
+    block.innerHTML = "";
     block.append(h1);
 }
