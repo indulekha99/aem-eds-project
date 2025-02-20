8b9dc09c1dfced6c6553a9d569dfc08fdee72864
indulekha99 <indulekhask9@gmail.com>
2025-02-20 11:12:20 +0530

    demo comp indentation fix

--- blocks/demoComp/demoComp.js
+++ blocks/demoComp/demoComp.js
@@ -1,7 +1,17 @@
+// export default function decorate(block) {
+//     // console.log('block is',block);
+//     const h1 = document.createElement("h1");
+//     h1.textContent = 'hello';
+//     block.innerHTML = '';
+//     block.append(h1);
+// }
 export default function decorate(block) {
-    console.log('block is',block);
-    const h1 = document.createElement("h1");
-    h1.textContent="hello";
-    block.innerHTML = "";
+    // console.log('block is', block);
+  
+    const h1 = document.createElement('h1');
+    h1.textContent = 'hello';
+  
+    block.innerHTML = '';
     block.append(h1);
-}
+  }
+  
