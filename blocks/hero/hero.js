5ae7cf870192b89a7c67dd0f6c0acf0133460a93
indulekha99 <indulekhask9@gmail.com>
2025-02-20 14:54:05 +0530

    added command

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
     console.log("Block is ", block);
-    const h1 = document.createElement("h1");
-    h1.textContent = "Headline about online pharmacy service";
-    block.innerHTML = "";
-    block.append(h1);
-    const btn = document.createElement("button");
-    var btntext = document.createTextNode("Primary");
-    btn.appendChild(btntext);
-    blocks.appendChild(btn);
+    // const h1 = document.createElement("h1");
+    // h1.textContent = "Headline about online pharmacy service";
+    // block.innerHTML = "";
+    // block.append(h1);
+    // const btn = document.createElement("button");
+    // var btntext = document.createTextNode("Primary");
+    // btn.appendChild(btntext);
+    // blocks.appendChild(btn);
   } 
