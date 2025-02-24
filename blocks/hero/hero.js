f931f08b911a171014f9f154237107d9db4a15e1
inkumar_deloitte <inkumar@deloitte.com>
2025-02-24 10:55:02 +0530

    button creation

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,13 @@
 export default function decorate(block) {
-    const btn = getElementsByClassName("button")
-    btn[0].textContent = "Primary";
-    block.append(btn);
+    // const btn = getElementsByClassName("button")
+    // btn[0].textContent = "Primary";
+    // block.append(btn);
+    const customButton = document.createElement('button');
+    customButton.textContent = "Primary";
+    customButton.classList.add('custom-button');
+    customButton.addEventListener('click',()=>{
+      alert('click event');
+    })
+
+    block.appendChild(customButton);
   } 
