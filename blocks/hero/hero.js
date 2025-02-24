f4e8668a53c3d8364e983e545d05f9510ca94b77
inkumar_deloitte <inkumar@deloitte.com>
2025-02-24 11:06:41 +0530

    btn and text contents

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,6 +2,25 @@ export default function decorate(block) {
     // const btn = getElementsByClassName("button")
     // btn[0].textContent = "Primary";
     // block.append(btn);
+
+    const headerButton = document.createElement('button');
+    headerButton.textContent = "Lilly Medicine";
+    headerButton.classList.add('header-button');
+    headerButton.addEventListener('click',()=>{
+      alert('click event');
+    })
+    block.appendChild(headerButton);
+
+    const heading = document.createElement('h1');
+    heading.textContent = "Headline about online pharmacy service";
+    block.appendChild(heading);
+    // heading.classList.add('custom-button');
+
+    const subHeading = document.createElement('p');
+    subHeading.textContent = "LilyDirect makes it simple to get authentic Lilly medicines directly to you, if prescribed.";
+    // heading.classList.add('custom-button');
+    block.appendChild(subHeading);
+
     const customButton = document.createElement('button');
     customButton.textContent = "Primary";
     customButton.classList.add('custom-button');
